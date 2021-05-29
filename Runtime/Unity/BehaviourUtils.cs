26daa0060e60151f2fa89ef85712501957e46f60
Andrew Burke <nomnomab1@gmail.com>
2021-05-28 21:18:25 -0400

    (0.0.8): BehaviourUtils

---
+++ Runtime/Unity/BehaviourUtils.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace NomUtils.Unity {
+	public static class BehaviourUtils {
+		/// <summary>
+		/// Gets a possible RectTransform from a Behaviour.
+		/// </summary>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static RectTransform GetRectTransform(this Behaviour behaviour) {
+			return behaviour.transform as RectTransform;
+		}
+	}
+}
