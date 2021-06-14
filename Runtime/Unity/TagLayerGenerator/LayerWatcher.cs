15b5fe0f8e5f391af9d62dee344df0bf29e7ceea
Andrew Burke <nomnomab1@gmail.com>
2021-06-14 14:09:37 -0400

    (0.0.11): Removed unneeded scripts for generation

--- Runtime/Unity/TagLayerGenerator/LayerWatcher.cs
+++
@@ -1,9 +0,0 @@
-﻿// namespace NomUtils.Unity.TagLayerGenerator {
-// 	public class LayerWatcher: UnityEditor.AssetModificationProcessor {
-// 		private static string[] OnWillSaveAssets(string[] paths) {
-// 			LayerCache.Regenerate();
-// 			
-// 			return paths;
-// 		}
-// 	}
-// }
--- Runtime/Unity/TagLayerGenerator/TagWatcher.cs
+++
@@ -1,9 +0,0 @@
-﻿// namespace NomUtils.Unity.TagLayerGenerator {
-// 	public class TagWatcher: UnityEditor.AssetModificationProcessor {
-// 		private static string[] OnWillSaveAssets(string[] paths) {
-// 			TagCache.Regenerate();
-// 			
-// 			return paths;
-// 		}
-// 	}
-// }
