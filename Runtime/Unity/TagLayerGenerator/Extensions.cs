f3cfe24df9dcbd4022d74b669605fe8b00e4ae5f
Andrew Burke <nomnomab1@gmail.com>
2021-06-14 11:46:34 -0400

    (0.0.11): .Value() methods

---
+++ Runtime/Unity/TagLayerGenerator/Extensions.cs
@@ -0,0 +1,15 @@
+﻿namespace NomUtils.Unity.TagLayerGenerator {
+	public static class Extensions {
+		public static int Value(this UnityLayer layer) {
+			return (int)layer;
+		}
+		
+		public static int Value(this UnityLayerMask mask) {
+			return (int)mask;
+		}
+
+		public static string Value(this UnityTag tag) {
+			return UnityTagNames.Cache[tag];
+		}
+	}
+}
